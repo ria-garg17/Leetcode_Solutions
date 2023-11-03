31656aac5e281e3602313475faaec2d2586f1faf
ria-garg17 <72888835+ria-garg17@users.noreply.github.com>
2023-11-03 14:12:47 +0530

    Time: 4 ms (39%), Space: 8.1 MB (49.67%) - LeetHub

---
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        int currNum = 1;
+        int index = 0;
+
+        while (currNum <= n && index < target.size()) {
+            int num = target[index];
+            if (num == currNum) {
+                ans.push_back("Push");
+                currNum++;
+                index++;
+            } else {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+                currNum++;
+            }
+        }
+
+        return ans;
+    }
+};
